588fd0df3343ff337e949600d69087ec5eddcc45
Thiago Monteiro Dolabella <thiago@getrak.com.br>
2018-03-13 22:35:32 -0300

    Ajustes para valid o Storage

--- src/services/Storage.js
+++ src/services/Storage.js
@@ -1,8 +1,23 @@
+// Valid Storage;
+const storage = (function() {
+	var uid = Date.now();
+    var result;
+    
+	try {
+        localStorage.setItem(uid, uid);
+        result = parseInt(localStorage.getItem(uid), 0) === uid;
+		localStorage.removeItem(uid);
+		return result && localStorage;
+	} catch (exception) {
+        return false;
+    }
+}());
+
 // Adiciona um busca no localStorage;
-export const setSearch = (search) => localStorage.setItem('text-search', search)
+export const setSearch = (search) => storage && localStorage.setItem('text-search', search)
 
 // Retorna a busca;
-export const getSearch = () => localStorage.getItem('text-search')
+export const getSearch = () => storage && localStorage.getItem('text-search')
 
 // Limpa a busca;
-export const cleanSearch = () => localStorage.removeItem('text-search')
+export const cleanSearch = () => storage && localStorage.removeItem('text-search')
