e555306a6d0f368eea01f8ae15bafacbc7be6ae0
Thiago Monteiro Dolabella <thiago@getrak.com.br>
2018-03-13 22:24:22 -0300

    Ajuste para melhoria de legibilidade e melhoria nos padrões

--- src/scenes/Search/index.js
+++ src/scenes/Search/index.js
@@ -17,10 +17,7 @@ class Search extends Component {
     value: Storage.getSearch() || ''
   }
 
-  constructor(props){
-    super(props);
-    this.timeout =  0;
-  }
+  timeout = 0;
 
   componentDidMount() {
     this.state.value && this.onChange(this.state.value)
@@ -43,18 +40,19 @@ class Search extends Component {
     if (this.timeout) clearTimeout(this.timeout)
     
     this.timeout = setTimeout(async () => {
-
       if (value.trim()) {
         Storage.setSearch(value)
 
-        await BooksAPI.search(value).then(books => {
-          const bk = books.map(book => {
+        const booksSearch = await BooksAPI.search(value);
+
+        if (booksSearch && Array.isArray(booksSearch)) {
+          const bk = booksSearch.map(book => {
             const check = this.state.bookshelf.filter(bf => bf.id === book.id)[0]
             return check ? check : book
           })
           
-          this.setState({ books: books.error ? [] : bk, loading: false })
-        });
+          this.setState({ books: bk, loading: false })
+        }
       } else {
         this.setState({ books: [], loading: false })
       }
@@ -64,18 +62,17 @@ class Search extends Component {
   onChangeBook = async (ev, book) => {
     const value = ev.target.value;
     await this.onLoadingBook(book);
+    await BooksAPI.update(book, value);
 
-    BooksAPI.update(book, value).then(() => {
-      this.setState({
-        books: this.changeProps(book, {'shelf': value, 'loading': false}),
-        bookshelf: this.changeProps(book, {'shelf': value, 'loading': false}) 
-      });
+    this.setState({
+      books: this.changeProps(book, {shelf: value, loading: false}),
+      bookshelf: this.changeProps(book, {shelf: value, loading: false}) 
     });
   }
 
   onLoadingBook = (book) => {
     return new Promise(resolve => {
-      this.setState({ books: this.changeProps(book, {'loading': true}) }, () => {
+      this.setState({ books: this.changeProps(book, {loading: true}) }, () => {
         resolve()
       });
     });
@@ -83,7 +80,7 @@ class Search extends Component {
 
   changeProps = (book, props) => {
     return this.state.books.map(b => {
-      return b.id === book.id ? Object.assign({}, b, props) : b
+      return b.id === book.id ? {...b, props} : b
     });
   }
 
